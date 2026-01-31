cab8f2e0343c9970400d611f2b5b79f453a3ef7d
KannappanPWJS <skannappan13@yahoo.com>
2026-01-31 23:11:38 +0530

    Add files via upload
    Assignment Code Day 1

---
+++ AssignmentCode1.js
@@ -0,0 +1,10 @@
+let userName = "Kannappan"
+let organization = "SeqAT"
+let contactNumber = 9876543210
+let knowsAutomation = true
+let usesPlaywright
+console.log("My UserName is " +userName+" "+typeof(userName))
+console.log("My Organization is " +organization+" "+typeof(organization))
+console.log("My Contact Number is " +contactNumber+" "+typeof(contactNumber))
+console.log("Automation status: " +knowsAutomation+" "+typeof(knowsAutomation))
+console.log("Uses PlayWright: "+typeof(usesPlaywright))
